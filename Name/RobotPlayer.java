f4304c734a589f812e33b0d8cdf1836bbd359f3b
Zhenyang Feng <feng.6@C02NX1LDG083.local>
2022-01-20 15:34:19 -0500

    reference to RunMiner

--- Name/RobotPlayer.java
+++ Name/RobotPlayer.java
@@ -123,28 +123,7 @@ public strictfp class RobotPlayer {
 	 * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
 	 */
 	static void runMiner(RobotController rc) throws GameActionException {
-		// Try to mine on squares around us.
-		MapLocation me = rc.getLocation();
-		System.out.println(me.x + " " + me.y);
-		for (int dx = -1; dx <= 1; dx++) {
-			for (int dy = -1; dy <= 1; dy++) {
-				MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
-				// Notice that the Miner's action cooldown is very low.
-				// You can mine multiple times per turn!
-				while (rc.canMineGold(mineLocation)) {
-					rc.mineGold(mineLocation);
-				}
-				while (rc.canMineLead(mineLocation)) {
-					rc.mineLead(mineLocation);
-				}
-			}
-		}
-
-		// Also try to move randomly.
-		Direction dir = directions[rng.nextInt(directions.length)];
-		if (rc.canMove(dir)) {
-			rc.move(dir);
-		}
+		RunMiner go = new RunMiner(rc);
 	}
 
 	/**
@@ -169,4 +148,5 @@ public strictfp class RobotPlayer {
 			rc.move(dir);
 		}
 	}
+
 }
